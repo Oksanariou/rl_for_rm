906ad0bdba0b605a46bca243b85f547e6ae6f323
Oksana RIOU <Oksana.RIOU@amadeus.com>
2019-06-27 16:58:35 +0200

    benchmark

--- DQL/benchmark.py
+++ DQL/benchmark.py
@@ -2,19 +2,30 @@ from keras import backend as K
 import tensorflow as tf
 import timeit
 
-with K.tf.device('/cpu:1'):
-    config = tf.ConfigProto(device_count = {'CPU' : 1, 'GPU' : 1})
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
 
+with K.tf.device('/gpu:0'):
     random_image_gpu = tf.random_normal((100, 100, 100, 3))
     net_gpu = tf.layers.conv2d(random_image_gpu, 32, 7)
     net_gpu = tf.reduce_sum(net_gpu)
 
-    session = tf.Session(config=config)
+with K.tf.device('/cpu:0'):
+    random_image_cpu = tf.random_normal((100, 100, 100, 3))
+    net_cpu = tf.layers.conv2d(random_image_cpu, 32, 7)
+    net_cpu = tf.reduce_sum(net_cpu)
 
-    K.set_session(session)
+
+sess = tf.Session(config=config)
 
 def gpu():
-    session.run(net_gpu)
+    sess.run(net_gpu)
+
+def cpu():
+    sess.run(net_cpu)
 
 gpu_time = timeit.timeit('gpu()', number=10, setup="from __main__ import gpu")
 print(gpu_time)
+
+cpu_time = timeit.timeit('cpu()', number=10, setup="from __main__ import cpu")
+print(cpu_time)
