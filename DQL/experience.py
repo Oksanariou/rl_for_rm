375dec04b6f4813ae6734bfc7488f0e869076a19
Oksana RIOU <Oksana.RIOU@amadeus.com>
2019-06-28 09:13:32 +0200

    comment

--- DQL/experience.py
+++ DQL/experience.py
@@ -5,7 +5,7 @@ from keras.losses import mean_squared_error, logcosh
 # from dynamic_programming_env_DCP import dynamic_programming_env_DCP
 
 # from DQL.agent import DQNAgent
-from DQL.agent import DQNAgent
+from agent import DQNAgent
 # from DQL.callbacks import TrueCompute, VDisplay, RevenueMonitor, RevenueDisplay, AgentMonitor, QCompute, QErrorDisplay, \
 #     QErrorMonitor, PolicyDisplay, MemoryMonitor, MemoryDisplay, BatchMonitor, BatchDisplay, TotalBatchDisplay, \
 #     SumtreeMonitor, SumtreeDisplay
