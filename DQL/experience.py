875c6f83fd276c0a89ed5c978bf2f20285f8db5c
Oksana RIOU <Oksana.RIOU@amadeus.com>
2019-06-28 09:26:01 +0200

    test_time

--- DQL/experience.py
+++ DQL/experience.py
@@ -16,6 +16,49 @@ from keras.layers import K
 
 import os
 
+import timeit
+
+def test_time():
+    data_collection_points = 20
+    micro_times = 5
+    capacity = 20
+    actions = tuple(k for k in range(50, 231, 20))
+    alpha = 0.8
+    lamb = 0.7
+
+    env = gym.make('gym_RMDCP:RMDCP-v0', data_collection_points=data_collection_points, capacity=capacity,
+                   micro_times=micro_times, actions=actions, alpha=alpha, lamb=lamb)
+
+    # Parameters:
+    parameters_dict = {}
+    parameters_dict["env"] = env
+    parameters_dict["replay_method"] = "DDQL"
+    parameters_dict["batch_size"] = 1024
+    parameters_dict["memory_size"] = 5000
+    parameters_dict["prioritized_experience_replay"] = False
+    parameters_dict["target_model_update"] = 100
+    parameters_dict["hidden_layer_size"] = 50
+    parameters_dict["dueling"] = False
+    parameters_dict["loss"] = mean_squared_error
+    parameters_dict["learning_rate"] = 0.001
+    parameters_dict["epsilon"] = 1.0
+    parameters_dict["epsilon_min"] = 0.02
+    parameters_dict["epsilon_decay"] = 0.9995
+    parameters_dict["state_weights"] = None
+
+    agent = DQNAgent(env=parameters_dict["env"],
+                     # state_scaler=env.get_state_scaler(), value_scaler=env.get_value_scaler(),
+                     replay_method=parameters_dict["replay_method"], batch_size=parameters_dict["batch_size"],
+                     memory_size=parameters_dict["memory_size"],
+                     prioritized_experience_replay=parameters_dict["prioritized_experience_replay"],
+                     target_model_update=parameters_dict["target_model_update"],
+                     hidden_layer_size=parameters_dict["hidden_layer_size"], dueling=parameters_dict["dueling"],
+                     loss=parameters_dict["loss"], learning_rate=parameters_dict["learning_rate"],
+                     epsilon=parameters_dict["epsilon"], epsilon_min=parameters_dict["epsilon_min"],
+                     epsilon_decay=parameters_dict["epsilon_decay"],
+                     state_weights=parameters_dict["state_weights"])
+    agent.init_network_with_true_Q_table()
+
 
 if __name__ == '__main__':
     print(os.getpid())
@@ -62,7 +105,7 @@ if __name__ == '__main__':
     nb_episodes = 10_000
 
     # agent.init_target_network_with_true_Q_table()
-    agent.init_network_with_true_Q_table()
+    print(timeit.timeit('exp.test_time()', number=10, setup="import DQL.experience as exp"))
 
     # before_train = lambda episode: episode == 0
     # every_episode = lambda episode: True
