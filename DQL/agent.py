a44abccaaf9f338a884a586b317154aeedf53b22
Oksana RIOU <Oksana.RIOU@amadeus.com>
2019-07-10 13:59:46 +0200

    getting rid of _ in agent

--- DQL/agent.py
+++ DQL/agent.py
@@ -299,7 +299,7 @@ class DQNAgent:
 
         tol = 10
         epochs = 10
-        while error > tol and total_epochs <= 3_000:
+        while error > tol and total_epochs <= 3000:
             self.init(states, true_Q_table, epochs)
             Q_table = self.compute_q_table()
             error = np.sqrt(np.square(true_Q_table - Q_table).sum())
