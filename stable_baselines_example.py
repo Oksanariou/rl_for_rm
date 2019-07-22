83ff76920addcbe0fc06d25285d0abfb501b579e
Oksana RIOU <Oksana.RIOU@amadeus.com>
2019-07-22 10:17:29 +0200

    test if gpu used

--- stable_baselines_example.py
+++ stable_baselines_example.py
@@ -290,10 +290,10 @@ def compare_computation_time(file_name, general_dir_name, parameters_dict, param
 
 if __name__ == '__main__':
 
-    results_path = Path("../Computation_time_without_gpu")
+    results_path = Path("../Computation_time_with_gpu")
     results_path.mkdir(parents=True, exist_ok=True)
 
-    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+    # os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
     if tf.test.gpu_device_name():
         print('GPU found')
