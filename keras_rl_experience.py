dd9f958a4b679d8c6d04693d1b2bd5d0fb42840b
Oksana RIOU <Oksana.RIOU@amadeus.com>
2019-10-07 08:52:14 +0200

    plot curves

--- keras_rl_experience.py
+++ keras_rl_experience.py
@@ -196,8 +196,8 @@ if __name__ == '__main__':
         parameter_values = [True, False]
         experience_name = Path("../Results/global_env") / Path(parameter_name)
         experience_name.mkdir(parents=True, exist_ok=True)
-        parameter_experience(experience_name, parameter_name, parameter_values, env_builder, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
-        plot_comparison(experience_name, parameter_values, env, absc, true_revenues)
+        # parameter_experience(experience_name, parameter_name, parameter_values, env_builder, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
+        plot_comparison(experience_name, parameter_values, env_single_agent, absc, true_revenues)
     except Exception:
         pass
     try:
@@ -205,8 +205,8 @@ if __name__ == '__main__':
         parameter_values = [32, 128, 256, 512, 1024]
         experience_name = Path("../Results/global_env") / Path(parameter_name)
         experience_name.mkdir(parents=True, exist_ok=True)
-        parameter_experience(experience_name, parameter_name, parameter_values, env_builder, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
-        plot_comparison(experience_name, parameter_values, env, absc, true_revenues)
+        # parameter_experience(experience_name, parameter_name, parameter_values, env_single_agent, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
+        plot_comparison(experience_name, parameter_values, env_single_agent, absc, true_revenues)
     except Exception:
         pass
     try:
@@ -214,8 +214,8 @@ if __name__ == '__main__':
         parameter_values = [10, 50, 100, 200, 300]
         experience_name = Path("../Results/global_env") / Path(parameter_name)
         experience_name.mkdir(parents=True, exist_ok=True)
-        parameter_experience(experience_name, parameter_name, parameter_values, env_builder, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
-        plot_comparison(experience_name, parameter_values, env, absc, true_revenues)
+        # parameter_experience(experience_name, parameter_name, parameter_values, env_builder, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
+        plot_comparison(experience_name, parameter_values, env_single_agent, absc, true_revenues)
     except Exception:
         pass
     try:
@@ -223,8 +223,8 @@ if __name__ == '__main__':
         parameter_values = [1, 2, 3, 4, 5]
         experience_name = Path("../Results/global_env") / Path(parameter_name)
         experience_name.mkdir(parents=True, exist_ok=True)
-        parameter_experience(experience_name, parameter_name, parameter_values, env_builder, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
-        plot_comparison(experience_name, parameter_values, env, absc, true_revenues)
+        # parameter_experience(experience_name, parameter_name, parameter_values, env_builder, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
+        plot_comparison(experience_name, parameter_values, env_single_agent, absc, true_revenues)
     except Exception:
         pass
     try:
@@ -232,8 +232,8 @@ if __name__ == '__main__':
         parameter_values = [True, False]
         experience_name = Path("../Results/global_env") / Path(parameter_name)
         experience_name.mkdir(parents=True, exist_ok=True)
-        parameter_experience(experience_name, parameter_name, parameter_values, env_builder, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
-        plot_comparison(experience_name, parameter_values, env, absc, true_revenues)
+        # parameter_experience(experience_name, parameter_name, parameter_values, env_builder, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
+        plot_comparison(experience_name, parameter_values, env_single_agent, absc, true_revenues)
     except Exception:
         pass
     try:
@@ -241,8 +241,8 @@ if __name__ == '__main__':
         parameter_values = [0.01, 0.1, 10, 100, 1000]
         experience_name = Path("../Results/global_env") / Path(parameter_name)
         experience_name.mkdir(parents=True, exist_ok=True)
-        parameter_experience(experience_name, parameter_name, parameter_values, env_builder, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
-        plot_comparison(experience_name, parameter_values, env, absc, true_revenues)
+        # parameter_experience(experience_name, parameter_name, parameter_values, env_builder, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
+        plot_comparison(experience_name, parameter_values, env_single_agent, absc, true_revenues)
     except Exception:
         pass
     try:
@@ -250,8 +250,8 @@ if __name__ == '__main__':
         parameter_values = [0.05, 0.1, 0.2, 0.3, 0.4]
         experience_name = Path("../Results/global_env") / Path(parameter_name)
         experience_name.mkdir(parents=True, exist_ok=True)
-        parameter_experience(experience_name, parameter_name, parameter_values, env_builder, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
-        plot_comparison(experience_name, parameter_values, env, absc, true_revenues)
+        # parameter_experience(experience_name, parameter_name, parameter_values, env_builder, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
+        plot_comparison(experience_name, parameter_values, env_single_agent, absc, true_revenues)
     except Exception:
         pass
     try:
@@ -259,8 +259,8 @@ if __name__ == '__main__':
         parameter_values = [1e-1, 1e-2, 1e-3, 1e-4]
         experience_name = Path("../Results/global_env") / Path(parameter_name)
         experience_name.mkdir(parents=True, exist_ok=True)
-        parameter_experience(experience_name, parameter_name, parameter_values, env_builder, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
-        plot_comparison(experience_name, parameter_values, env, absc, true_revenues)
+        # parameter_experience(experience_name, parameter_name, parameter_values, env_builder, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
+        plot_comparison(experience_name, parameter_values, env_single_agent, absc, true_revenues)
     except Exception:
         pass
     try:
@@ -268,8 +268,8 @@ if __name__ == '__main__':
         parameter_values = [1000, 5000, 10000, 50000, 100000]
         experience_name = Path("../Results/global_env") / Path(parameter_name)
         experience_name.mkdir(parents=True, exist_ok=True)
-        parameter_experience(experience_name, parameter_name, parameter_values, env_builder, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
-        plot_comparison(experience_name, parameter_values, env, absc, true_revenues)
+        # parameter_experience(experience_name, parameter_name, parameter_values, env_builder, nb_timesteps, true_revenues, absc, nb_runs, callback_frequency)
+        plot_comparison(experience_name, parameter_values, env_single_agent, absc, true_revenues)
     except Exception:
         pass
 
