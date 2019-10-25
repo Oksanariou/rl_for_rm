1346df4500a65aa9b8ef166cec618616b5899701
Oksana RIOU <Oksana.RIOU@amadeus.com>
2019-10-25 14:11:21 +0200

    typo

--- keras_rl_experience.py
+++ keras_rl_experience.py
@@ -423,7 +423,7 @@ if __name__ == '__main__':
     true_revenue1, true_revenue2, true_bookings, true_bookings_flight1, true_bookings_flight2, true_prices_proposed_flight1, true_prices_proposed_flight2 = env.average_n_episodes(
             true_P, 10000)
 
-    experience_name = Path("../Results/2D_individual_rewards/0.6")
+    experience_name = Path("../Results/2D_shared_rewards/1.8")
     experience_name.mkdir(parents=True, exist_ok=True)
     # run_n_times(env_param, experience_name, global_env_builder, param_dict, nb_timesteps, number_of_runs, callback_frequency)
     list_of_rewards, mean_revenues1, mean_revenues2, mean_bookings, mean_bookings1, mean_bookings2, mean_prices_proposed1, mean_prices_proposed2 = env.collect_list_of_mean_revenues_and_bookings(experience_name)
@@ -665,18 +665,18 @@ if __name__ == '__main__':
     # V = q_to_v(env, Q_table).reshape(env.observation_space.nvec)
 
     # revenues = np.array(callback.rewards)
-    for k in range(20):
-        plt.figure()
-        axes = plt.gca()
-        plt.plot(absc, [true_revenue1 + true_revenue2] * len(absc), 'g--', label="Optimal")
-        plt.plot(absc, np.array(list_of_rewards[k][:,0]) + np.array(list_of_rewards[k][:,1]), color=parameters[configuration]["color"], label="Independent agents")
-        plt.plot(absc, list_of_rewards[k][:,0], color="orange", label="Flight1")
-        plt.plot(absc, list_of_rewards[k][:,1], color="blue", label="Flight2")
-        plt.legend(loc='best')
-        plt.xlabel("Number of steps")
-        plt.ylabel("Average revenue")
-        # axes.set_ylim([0, 265])
-        plt.savefig('../Results/independent_agents_multi_flights_revenue'+str(k)+'.png')
+    # for k in range(20):
+    #     plt.figure()
+    #     axes = plt.gca()
+    #     plt.plot(absc, [true_revenue1 + true_revenue2] * len(absc), 'g--', label="Optimal")
+    #     plt.plot(absc, np.array(list_of_rewards[k][:,0]) + np.array(list_of_rewards[k][:,1]), color=parameters[configuration]["color"], label="Independent agents")
+    #     plt.plot(absc, list_of_rewards[k][:,0], color="orange", label="Flight1")
+    #     plt.plot(absc, list_of_rewards[k][:,1], color="blue", label="Flight2")
+    #     plt.legend(loc='best')
+    #     plt.xlabel("Number of steps")
+    #     plt.ylabel("Average revenue")
+    #     # axes.set_ylim([0, 265])
+    #     plt.savefig('../Results/independent_agents_multi_flights_revenue'+str(k)+'.png')
 
     #
     # indx_nb = 6
@@ -707,7 +707,7 @@ if __name__ == '__main__':
     plt.title("Overall load factor: {:.2}".format((np.sum(bookings1) + np.sum(bookings2)) / (env.C1 + env.C2)))
     plt.legend()
     plt.xticks(env.prices_flight1)
-    plt.savefig('../Results/collaborative_agents_multi_flights_bookings_low_demand.png')
+    plt.savefig('../Results/collaborative_agents_multi_flights_bookings_high_demand.png')
 
     plt.figure()
     width = 5
@@ -724,4 +724,4 @@ if __name__ == '__main__':
     plt.ylabel("Proposed fares")
     plt.legend()
     plt.xticks(env.prices_flight1)
-    plt.savefig('../Results/collaborative_agents_multi_flights_proposed_fares_low_demand.png')
+    plt.savefig('../Results/collaborative_agents_multi_flights_proposed_fares_high_demand.png')
