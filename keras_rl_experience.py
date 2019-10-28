89c850b96f6645c02911d141d3dbf5e7d14d5515
Oksana RIOU <Oksana.RIOU@amadeus.com>
2019-10-28 15:08:01 +0100

    dr of 0.7

--- keras_rl_experience.py
+++ keras_rl_experience.py
@@ -381,7 +381,7 @@ if __name__ == '__main__':
         # list_difference_to_true_revenue_mnl = []
         list_final_revenues = [[] for k in range(len(demand_ratios))]
         for dr_idx in range(len(demand_ratios)):
-            if dr_idx == 1:
+            if dr_idx == 2:
                 env_param = multiagent_env_parameters_dict()
                 env_param["demand_ratio"] = demand_ratios[dr_idx]
                 env = global_env_builder(env_param)
@@ -414,7 +414,7 @@ if __name__ == '__main__':
     plt.legend(loc='best')
     plt.xlabel("Training steps")
     plt.ylabel("Relative revenue")
-    plt.savefig("../Results/comparison_demand_ratio_0.6.png")
+    plt.savefig("../Results/comparison_demand_ratio_0.7.png")
             #
             # plt.figure()
             # width = 5
